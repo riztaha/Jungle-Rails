2680d49cd6c9c2c167afaef4855ff8ff6675f06c
Taha Rizvi <riztaha@gmail.com>
2020-05-01 03:52:15 +0000

    Added Admin Dashboard page to show inventory/category counts & a button to view admin-products page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASS"]
 
   def show
+    @categories = Category.count
+    @inventory = Product.count
   end
 end
