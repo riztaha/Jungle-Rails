4991e4f7023cb904ec6d51b61e13c10ba1d5a64f
Taha Rizvi <riztaha@gmail.com>
2020-05-02 02:53:55 +0000

    added who the current user is to the app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,30 +3,44 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  # Authorize is for sending someone to the login page if they aren't logged in -
+  # this is how we keep certain pages our site secure...
+  # user's have to login before seeing them.
+  # def authorize
+  #   redirect_to '/login' unless current_user
+  # end
+
   private
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
+
   helper_method :cart
 
   def enhanced_cart
-    @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+    @enhanced_cart ||= Product.where(id: cart.keys).map { |product| { product: product, quantity: cart[product.id.to_s] } }
   end
+
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
-    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
+    enhanced_cart.map { |entry| entry[:product].price_cents * entry[:quantity] }.sum
   end
-  helper_method :cart_subtotal_cents
 
+  helper_method :cart_subtotal_cents
 
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
-      expires: 10.days.from_now
+      expires: 10.days.from_now,
     }
     cookies[:cart]
   end
-
 end
