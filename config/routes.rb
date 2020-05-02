2d91a9d463ac43e4f67dfab1ddfc6fa08c278531
Taha Rizvi <riztaha@gmail.com>
2020-05-02 02:48:34 +0000

    aded login/logout sessions to the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
 
   get "about" => "about#show"
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
   get "/signup" => "users#new"
   post "/users" => "users#create"
 
